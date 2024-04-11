3498bf0ce63c796a21698ef41b399b1dc3375b60
SoyNexoo <soynexoo@gmail.com>
2024-04-11 04:47:30 -0300

    Refactor: Se añadio carpetas importantes. Hooks, Constantes, Tipos, Lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import { CollaborativeApp } from "@/utils/CollaborativeApp";
-import { Room } from "@/utils/Room";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <Room>
-    <CollaborativeApp></CollaborativeApp>
-   </Room>
+    <h1 className="text-5xl">Liveblocks Figma Clone</h1>
   );
 }
