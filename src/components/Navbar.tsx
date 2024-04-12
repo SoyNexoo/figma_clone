b12bde662bf0506a44e3966f7f94bf292fcc4f7d
SoyNexoo <soynexoo@gmail.com>
2024-04-12 20:51:03 -0300

    delete logo navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,6 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
-      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
-
       <ul className="flex flex-row">
         {navElements.map((item: ActiveElement | any) => (
           <li
