3f5b27e34fd37b1f080ef48cb56522ddc5473df7
RomanFama592 <107817690+RomanFama592@users.noreply.github.com>
2024-04-16 02:41:44 +0000

    add: smoothing of pointers

--- src/components/cursor/Cursor.tsx
+++ src/components/cursor/Cursor.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 const Cursor = ({ color, x, y, message }: Props) => {
   return (
-    <div className="pointer-events-none absolute top-0 left-0" style={{ transform: `translateX(${x}px) translateY(${y}px)` }}>
+    <div className="pointer-events-none absolute top-0 left-0 transition-transform" style={{ transform: `translateX(${x}px) translateY(${y}px)` }}>
       <CursorSVG color={color} />
       {message && (
         <div className='absolute left-2 top-5 rounded-3xl px-4 py-2' style={{ backgroundColor: color, borderRadius: 20 }}>
